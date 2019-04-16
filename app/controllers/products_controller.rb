01b14678bbe61fd592fdb91514b9fd80525aad88
Zachary Lehmann <zlehmann@gmail.com>
2019-04-16 01:03:49 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,7 +18,7 @@ class ProductsController < ApplicationController
     redirect_to products_path
   end
 
-  def body
+  def description
     product = Product.find(params[:id])
     render plain: product.body
   end
