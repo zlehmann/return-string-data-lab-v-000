4dad86be1e152a1ea554bab709c86fcd0c372240
Zachary Lehmann <zlehmann@gmail.com>
2019-04-16 01:03:57 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :orders
   resources :invoices
   resources :products
-  get '/products/:id/body', to: 'products#body'
+  get '/products/:id/description', to: 'products#description'
   get '/products/:id/inventory', to: 'products#inventory'
 end
